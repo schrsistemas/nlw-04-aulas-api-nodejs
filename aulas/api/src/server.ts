782e2bc448bfc5e7ef7ad950d6723f0a4c13f3ed
Daniel Schroeder <schrsistemas@gmail.com>
2021-02-22 21:16:17 -0300

    1º Aula: Rumo ao próximo nível - NLW#4 @Rocketseat  - Aprendendo os conceitos básicos do que é uma API;  - Conhecendo NodeJS, Typescript e Insomnia;  - Configurações e Ambientação;  - Colocando em prática alguns dos conceitos utilizados acesso a Requisições no servidor;

---
+++ aulas/api/src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELTE => Deletar
+ * PATCH => Alteração especifica
+ * 
+ */
+
+ // http://localhost:3333/users
+ //app.get("/users", (request, response) => {
+app.get("/", (request, response) => {
+        //return response.send("Hello World - NLW04")
+    return response.json({message: "Test Msg NLW04"})
+})
+
+// 1 param => Rota (Recurso API)
+// 2 param => request, response
+app.post("/", (request, response) => {
+    // Recebeu os dados para salvar
+
+    return response.json({message: "Os dados foram salvos com sucesso!"})
+})
+
+
+app.listen(3333, () => console.log("Server is running!"));
+
